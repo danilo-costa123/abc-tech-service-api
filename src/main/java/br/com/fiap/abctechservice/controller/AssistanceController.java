fe7f7f58887f4ebf9e56c0391d7e04f6609ac2ce
danilo-costa123 <danilo.sa01@gmail.com>
2022-04-06 21:43:48 -0300

    first commit

---
+++ src/main/java/br/com/fiap/abctechservice/controller/AssistanceController.java
@@ -0,0 +1,31 @@
+package br.com.fiap.abctechservice.controller;
+
+
+import br.com.fiap.abctechservice.application.AssistanceApplication;
+import br.com.fiap.abctechservice.application.dto.AssistDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/assistance")
+public class AssistanceController {
+
+    private final AssistanceApplication assistanceApplication;
+    public AssistanceController (
+            @Autowired
+                    AssistanceApplication assistanceApplication
+    ){
+        this.assistanceApplication = assistanceApplication;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<AssistDto>> getAssists() {
+        List<AssistDto> list = this.assistanceApplication.getAssists();
+        return ResponseEntity.ok(list);
+    }
+}
---
+++ src/main/java/br/com/fiap/abctechservice/controller/OrderController.java
@@ -0,0 +1,35 @@
+package br.com.fiap.abctechservice.controller;
+
+import br.com.fiap.abctechservice.application.OrderApplication;
+import br.com.fiap.abctechservice.application.dto.OrderDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+
+    private final OrderApplication orderApplication;
+
+
+    public OrderController(@Autowired OrderApplication orderApplication) {
+        this.orderApplication = orderApplication;
+    }
+
+    @PostMapping()
+    public ResponseEntity createOrder(
+            @Valid
+            @RequestBody
+            OrderDto orderDto
+    ){
+        orderApplication.createOrder(orderDto);
+        return ResponseEntity.ok().build();
+    }
+
+}
